afd6966208c4816deef006624b89f858e98ddad6
Kaiser-G <kaiserg6969@gmail.com>
2023-10-09 12:42:14 +0200

    Changements du 9 octobre 2023

--- Animation de texte JS 2/app.js
+++ Animation de texte JS 2/app.js
@@ -7,20 +7,19 @@ new Typewriter(txtAnim, {
 
   .changeDelay(20)
   .typeString(
-    "<span style='color: white'> Bonjour,<br> aujourd'hui nous allons<br> faire  une recette à la truffe :<br></span>"
+    "<span style='color: white'> Bonjour,<br> aujourd'hui nous allons faire une recette<br> de pommes de terre gratinées au four.<br></span>"
   )
   .pauseFor(300)
-  .typeString("<strong>Dev Full-Stack<strong> !")
-  .pauseFor(1000)
-  .deleteChars(13)
-  .typeString("<span style='color: #27ae60'> CSS</span> !")
-  .pauseFor(1000)
-  .deleteChars(5)
-  .typeString("<span style='color: midnightblue'> React</span> !")
-  .pauseFor(1000)
-  .deleteChars(7)
-  .typeString("<span style='color: red'> HTML</span> !")
-  .pauseFor(1000)
-  .deleteChars(12)
+  .typeString("<span style='color: white'> Ingrédients :</span>")
+  .pauseFor(2000)
+  .typeString("<span style='color: white'> 4 grosses pommes de terre</span>")
+  .pauseFor(2000)
+  .deleteChars(26)
+  .typeString("<span style='color: white'> 100 g de beurre</span>")
+  .pauseFor(2000)
+  .deleteChars(16)
+  .typeString("<span style='color: white'> Sel et poivre</span>")
+  .pauseFor(2000)
+  .deleteChars(28)
   .typeString("<span style='color: white'> <br>Bon Appétit</span>")
   .start();
